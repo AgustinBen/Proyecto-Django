887eb778cab8c176181956dee35226272b5190f8
AgustinBen <agustinbengolea97@gmail.com>
2022-10-02 10:25:10 -0300

    tercer commit

--- miproyecto_cdw/desafios/views.py
+++ miproyecto_cdw/desafios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -15,21 +16,32 @@ meses = {
     'septiembre': 'lee tres libros',
     'octubre': 'Practica Python',
     'noviembre': 'Practica Python',
-    'diciembre': 'lee cuatro libros',
+    'diciembre': False,
 }
 
 def index(request):
     # return HttpResponse('hola mundo!')
     pagina = ''
-    for mes in meses.keys():
-        pagina += '<a href=%s><h1>%s</h1></a>'%(mes, mes)
-    return HttpResponse(pagina)
+    # for mes in meses.keys():
+    #     pagina += '<a href= %s ><h1>%s</h1></a>'%(mes, mes)
+    # return HttpResponse(pagina)
+    return render(request, "desafios/index.html", {
+        'meses': meses.keys(),
+    })
+    
+
+
 
 
 def monthly_challenge(request, month):
     pagina = '<h1>%s</h1>'%(month)
     for mes in meses.keys():
         if month == mes:
-            pagina += '<p>%s</p>'%(meses[mes])
-            return HttpResponse(pagina)
+            # pagina += '<p>%s</p>'%(meses[mes])
+            # texto_desafio = render_to_string(“desafios/desafio.html”)
+            # return HttpResponse(pagina)
+            return render(request, "desafios/desafios.html", {
+                'month': month,
+                'texto': meses[mes],
+            })
     return HttpResponse('<h1>No se encontro el mes</h1>')
