c9fcab5a1af7003c7c57a2582cdc75e42964eded
AgustinBen <agustinbengolea97@gmail.com>
2022-10-31 19:53:37 -0300

    [ADD] carousel

--- E-commerce/Tienda/views.py
+++ E-commerce/Tienda/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse, Http404
 # Create your views here.
 
 
+productos = {
+    'Agus Mineral',
+    'Queso Azul',
+    'Fernet',
+}
 
 def index(request):
     return render(request, 'Tienda/index.html')
@@ -10,3 +15,7 @@ def index(request):
 
 def lista_productos(request):
     return render(request, 'Tienda/lista_productos.html')
+
+
+# def productos_index (request, item):
+#     pass
