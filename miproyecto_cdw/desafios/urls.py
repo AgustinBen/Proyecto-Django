0735bc78e3d4ee449d1c14b4f3bcc40cd6e72a45
AgustinBen <104386687+AgustinBen@users.noreply.github.com>
2023-05-21 08:43:54 -0300

    Delete miproyecto_cdw directory

--- miproyecto_cdw/desafios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
- #  path('enero', views.index)
-    path('', views.index),
-    path('<month>', views.monthly_challenge),
-]   
